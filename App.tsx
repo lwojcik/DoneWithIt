5dea9e08cf1beb706d9bf39aad9c6556b289a0bf
Łukasz Wójcik <lwojcik@lukem.net>
2021-01-27 21:36:54 +0100

    done: Getting Started

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native yo</Text>
+      <Text>Hello React Native</Text>
     </View>
   );
 }
